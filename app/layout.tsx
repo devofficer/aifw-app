e7cd5a9f1cfa4f3ffd34aef3c35fe312d231cf4f
devofficer <myeyesontech@gmail.com>
2023-09-18 11:36:43 +0200

    feat: navbar customization

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,39 @@
-import Navbar from '@/components/Navbar';
-import Footer from '@/components/Footer';
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export const dynamic = "force-dynamic";
 
-import '@/styles/globals.css'
+import "@/styles/globals.css";
 
 export const metadata = {
-  title: 'MaisonMeta',
-  description: 'Generative AI platform provided by MaisonMeta',
-}
+  title: "MaisonMeta",
+  description: "Generative AI platform provided by MaisonMeta",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+          rel="stylesheet"
+        />
       </head>
       <body>
-        <div className='container'>
-          <Navbar />
-        </div>
-        <main className="viewport">
-          {children}
-        </main>
+        <Navbar />
+        <main className="viewport">{children}</main>
         <Footer />
       </body>
     </html>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./app/**/*.{js,ts,jsx,tsx,mdx}'],
+  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       colors: {
-        'primary': '#DE6F4C',
-        'primary-600': '#CE5F3C',
-        'primary-500': '#BE4F2C',
-        'secondary': '#FFFFFF'
+        primary: "#C2FF04",
+        "primary-600": "#B2EF03",
+        "primary-500": "#A2DF03",
+        secondary: "#000000",
       },
       fontSize: {
-        sm: '14px',
-        md: '16px',
-        base: '18px',
-        xl: '25px',
-        '2xl': '38px',
-      }
+        sm: "14px",
+        md: "16px",
+        base: "18px",
+        xl: "25px",
+        "2xl": "38px",
+      },
     },
   },
   plugins: [],
-}
+};
